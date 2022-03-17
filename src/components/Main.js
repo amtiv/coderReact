5a929ccfa3cc95bb7cea42851a3abc458de6d865
amtiv <89408740+amtiv@users.noreply.github.com>
2022-03-16 22:10:53 -0300

    Desafío Entrega Intermedia
    Desafío Entrega Intermedia

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import React from "react";
 import ItemListContainer from "./ItemListContainer";
 import ItemDetailContainer from "./ItemDetailContainer";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Carrito from "./Carrito";
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
   return (
@@ -13,6 +15,7 @@ const Main = () => {
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/item/:slug" element={<ItemDetailContainer />} />
       </Routes>
+      <ToastContainer />
     </main>
   );
 };
